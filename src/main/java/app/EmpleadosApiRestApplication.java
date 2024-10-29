50458b9a234e1fac1086ad9fb971db00a8070782
ArielAVega <141455765+ArielAVega@users.noreply.github.com>
2024-10-29 15:49:20 -0300

    Create the project. Initial configuration

---
+++ src/main/java/app/EmpleadosApiRestApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadosApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadosApiRestApplication.class, args);
+	}
+
+}
